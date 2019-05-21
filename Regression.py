fb5a47c9679327193e40a7e73bf844dce593c7c6
한일석 <x2ever@naver.com>
2019-05-22 00:39:23 +0900

    Update Regression.py

--- Regression.py
+++ Regression.py
@@ -4,8 +4,11 @@ from pyspark.mllib.regression import LabeledPoint, LinearRegressionWithSGD, Line
 import numpy as np
 
 def parsePoint(line):
-    values = [float(x) for x in line.replace(',', ' ').split(' ')]
-    return LabeledPoint(values[0], values[1:])
+    try:
+        values = [float(x) for x in line.replace(',', ' ').split(' ')]
+        return LabeledPoint(values[6], values[:6])
+    except:
+        pass
 
 sc= SparkContext()
 train_data = sc.textFile("train.csv")
@@ -27,23 +30,18 @@ valuesAndPredsRidgeTest = parsedTestData.map(lambda p: (p.label, model_ridge.pre
 valuesAndPredsLassoTrain = parsedTrainData.map(lambda p: (p.label, model_lasso.predict(p.features)))
 valuesAndPredsLassoTest = parsedTestData.map(lambda p: (p.label, model_lasso.predict(p.features)))
 
-RMSE_Linear_Train = np.sqrt(valuesAndPredsLinearTrain.map(lambda vp: (vp[0] - vp[1])**2).reduce(lambda x, y: x + y) / valuesAndPredsLinearTrain.count())
-RMSE_Linear_Test = np.sqrt(valuesAndPredsLinearTest.map(lambda vp: (vp[0] - vp[1])**2).reduce(lambda x, y: x + y) / valuesAndPredsLinearTest.count())
+MSE_Linear = (valuesAndPredsLinearTrain.map(lambda vp: (vp[0] - vp[1])**2).reduce(lambda x, y: x + y) + valuesAndPredsLinearTest.map(lambda vp: (vp[0] - vp[1])**2).reduce(lambda x, y: x + y)) / \
+    (valuesAndPredsLinearTrain.count() + valuesAndPredsLinearTest.count())
 
-RMSE_Ridge_Train = np.sqrt(valuesAndPredsRidgeTrain.map(lambda vp: (vp[0] - vp[1])**2).reduce(lambda x, y: x + y) / valuesAndPredsRidgeTrain.count())
-RMSE_Ridge_Test = np.sqrt(valuesAndPredsRidgeTest.map(lambda vp: (vp[0] - vp[1])**2).reduce(lambda x, y: x + y) / valuesAndPredsRidgeTest.count())
+MSE_Ridge = (valuesAndPredsRidgeTrain.map(lambda vp: (vp[0] - vp[1])**2).reduce(lambda x, y: x + y) + valuesAndPredsRidgeTest.map(lambda vp: (vp[0] - vp[1])**2).reduce(lambda x, y: x + y)) / \
+    (valuesAndPredsRidgeTrain.count() + valuesAndPredsRidgeTest.count())
 
-RMSE_Lasso_Train = np.sqrt(valuesAndPredsLassoTrain.map(lambda vp: (vp[0] - vp[1])**2).reduce(lambda x, y: x + y) / valuesAndPredsLassoTrain.count())
-RMSE_Lasso_Test = np.sqrt(valuesAndPredsLassoTest.map(lambda vp: (vp[0] - vp[1])**2).reduce(lambda x, y: x + y) / valuesAndPredsLassoTest.count())
+MSE_Lasso = (valuesAndPredsLassoTrain.map(lambda vp: (vp[0] - vp[1])**2).reduce(lambda x, y: x + y) + valuesAndPredsLassoTest.map(lambda vp: (vp[0] - vp[1])**2).reduce(lambda x, y: x + y)) / \
+    (valuesAndPredsLassoTrain.count() + valuesAndPredsLassoTest.count())
 
-print("Root Mean Squared Error of 'Linear Model + Train Set': " + str(RMSE_Linear_Train))
-print("Root Mean Squared Error of 'Linear Model + Test Set': " + str(RMSE_Linear_Test))
-
-print("Root Mean Squared Error of 'Ridge Model + Train Set': " + str(RMSE_Ridge_Train))
-print("Root Mean Squared Error of 'Ridge Model + Test Set': " + str(RMSE_Ridge_Test))
-
-print("Root Mean Squared Error of 'Lasso Model + Train Set': " + str(RMSE_Lasso_Train))
-print("Root Mean Squared Error of 'Lasso Model + Test Set': " + str(RMSE_Lasso_Test))
+print("Root Mean Squared Error of 'Linear Model + Total Data Set': " + str(np.sqrt(MSE_Linear)))
+print("Root Mean Squared Error of 'Ridge Model + Total Data Set': " + str(np.sqrt(MSE_Ridge)))
+print("Root Mean Squared Error of 'Lasso Model + Total Data Set': " + str(np.sqrt(MSE_Lasso)))
 
 model_lenear.save(sc, "2014312051_linear")
 model_ridge.save(sc, "2014312051_ridge")
